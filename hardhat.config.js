6cc66fdfbb7fc1a1da273996dee3448a62e8e6e1
saira512dev <512saira@gmail.com>
2022-08-15 11:23:14 +0530

    Smart contract tested, deployed and verified

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,21 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-chai-matchers");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const API_URL = process.env.STAGING_INFURA_URL;
+const PRIVATE_KEY = process.env.STAGING_PRIVATE_KEY;
+
+
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.4",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: API_URL,
+      accounts:[`0x${PRIVATE_KEY}`],
+      gas: 2100000,
+      gasPrice: 8000000000,
+    },
+  },
 };
